4215cdb18a8e4afcc1ef1bfc428ffc1729d1bca6
Aidar Abdzhanov <abdzhaid@student.cvut.cz>
2025-04-29 11:05:39 +0200

    fix

--- work/uart-calc-add/uart-calc-add.c
+++ work/uart-calc-add/uart-calc-add.c
@@ -139,12 +139,7 @@ int main(int argc, char *argv[])
     }
   }
 
-  while (1){
-    if (mem[write_statuus_o] == SERP_TX_ST_REG_READY_m){
-      mem[write_data_o] = '\n';
-      break;
-  }
-}
+
 
 
   while (result > 0){
@@ -153,7 +148,12 @@ int main(int argc, char *argv[])
       result /= 10;
     }
   }
-
+  while (1){
+    if (mem[write_statuus_o] == SERP_TX_ST_REG_READY_m){
+      mem[write_data_o] = '\n';
+      break;
+  }
+}
 
   return 0;
 }
