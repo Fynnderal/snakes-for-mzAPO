1208d66c50d293771ff52938a741725fae218df1
abdzhaid <abdzhaid@bullseye>
2025-05-06 14:59:13 +0200

    controls for snake were added

--- sem_prace/mzapo_template/change_me.c
+++ sem_prace/mzapo_template/change_me.c
@@ -6,18 +6,22 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
 #include "mzapo_regs.h"
 
 unsigned short* fb;
+volatile uint32_t otoc;
+unsigned char *mem_base;
 
 enum {
-  UP = 0,
+  DOWN = 0,
   RIGHT,
+  UP,
   LEFT,
-  DOWN,
+  NONE
 } typedef Directions;
 
 struct {
@@ -30,7 +34,7 @@ struct {
     int currentY;
     int sectionSize;
     int sectionsNumber;
-    int speed;
+    double speedFactor;
     section* sections;
     Directions* directions;
 } typedef snake;
@@ -50,8 +54,13 @@ void move_snake(snake* player_snake);
 
 void snake_init(snake* player_snake, unsigned char* parlcd_mem_base);
 
+void draw_on_screen();
+
+void clean_screen();
+
+bool checkConrools(int cotrolID, snake* player_snake, long long now, long long previous_update);
+
 int main(int argc, char *argv[]) {
-  unsigned char *mem_base;
   unsigned char *parlcd_mem_base;
   uint32_t val_line=5;
   unsigned int c;
@@ -84,6 +93,8 @@ int main(int argc, char *argv[]) {
   parlcd_hx8357_init(parlcd_mem_base);
 
   //initialization of the snake
+  otoc = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o); 
+
   section *sections = (section*) malloc(10 * sizeof(section));
   Directions * directions  = (Directions* ) malloc (16 * sizeof(Directions));
 
@@ -91,35 +102,55 @@ int main(int argc, char *argv[]) {
     exit(-1);
   }
 
-  for (int i = 0; i < 16; i++){
+  for (int i = 0; i < 15; i++){
+
     directions[i] = DOWN;
   }
+  directions[15] = RIGHT;
 
-  snake red_snake = {10, 100, 8, 10, 5, sections, directions};
+  snake red_snake = {10, 100, 8, 10, 1, sections, directions};
   
 
 
+  snake_init(&red_snake, parlcd_mem_base);
+  struct timespec ts;
 
+
+  long long now;
+  long long previousUpdateTime = 0;
+  long long timeBetweenUpdates = 1000;
+
+  long long previousCheckControls = 0;
+
+  int previousDelta = 0;
   while (true){
-    clean_screen()
-    move_snake(&red_snake);
-    for (int i = 1; i < 16; i++){
-      red_snake.directions[i] = red_snake.directions[i - 1];
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+    now = (long long) ts.tv_sec * 1000 + (long long)ts.tv_nsec / 1000000;
+    
+    if (checkConrools(0, &red_snake, now, previousCheckControls)) 
+      previousCheckControls = now;
+
+    if (now - previousUpdateTime >= timeBetweenUpdates){
+      clean_screen();
+      move_snake(&red_snake);
+
+      draw_snake(&red_snake, parlcd_mem_base);
+      previousUpdateTime = now;
     }
 
-    draw_snake(&red_snake, parlcd_mem_base);
-    sleep(0.5);
   }
 
 
   printf("Goodbye world\n");
+  
 
   return 0;
 }
 
 
 void snake_init(snake* player_snake, unsigned char* parlcd_mem_base){
-    for (int k = 0; k < player_snake->sectionsNumber; k++){
+  for (int k = 0; k < player_snake->sectionsNumber; k++){
     player_snake->sections[k].currentX = player_snake->currentX;
     player_snake->sections[k].currentY = player_snake->currentY - k * player_snake->sectionSize;
   }
@@ -134,14 +165,10 @@ void snake_init(snake* player_snake, unsigned char* parlcd_mem_base){
     }
   }
 
-  parlcd_write_cmd(parlcd_mem_base, 0x2c);
-  for (int ptr = 0; ptr < 480*320 ; ptr++) {
-      parlcd_write_data(parlcd_mem_base, fb[ptr]);
-  }
-
+  draw_on_screen(parlcd_mem_base);
 }
 
-void draw_on_screen(){
+void draw_on_screen(unsigned char* parlcd_mem_base){
   parlcd_write_cmd(parlcd_mem_base, 0x2c);
   for (int ptr = 0; ptr < 480*320 ; ptr++) {
       parlcd_write_data(parlcd_mem_base, fb[ptr]);
@@ -165,6 +192,8 @@ void draw_snake(snake* player_snake, unsigned char* parlcd_mem_base){
       }
     }
   }
+
+  draw_on_screen(parlcd_mem_base);
 }
 
 void move_snake(snake* player_snake){
@@ -172,12 +201,66 @@ void move_snake(snake* player_snake){
 
     switch(player_snake->directions[i]) {
       case RIGHT:
-        printf("Got it\n");
+        player_snake->sections[i].currentX += player_snake->speedFactor * player_snake->sectionSize;
         break; 
 
+      case LEFT:
+        player_snake->sections[i].currentX -= player_snake->speedFactor * player_snake->sectionSize;
+        break;
+
       case DOWN:
-        player_snake->sections[i].currentY += player_snake->speed;
+        player_snake->sections[i].currentY += player_snake->speedFactor * player_snake->sectionSize;
+        break;
+
+      case UP:
+        player_snake->sections[i].currentY -= player_snake->speedFactor * player_snake->sectionSize;
+        break;
+      
+      case NONE:
+        printf("No directions for %d\n", i);
         break;
     }
   }
-} 
+
+  for (int i = 15; i > 0; i--){
+      player_snake->directions[i] = player_snake->directions[i - 1];
+  }
+
+}
+
+bool checkConrools(int cotrolID, snake* player_snake, long long now, long long previous_update){
+  volatile uint32_t novy_otoc = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o);
+  int delta = (int) ((novy_otoc >> 16)&0xff) - (int)((otoc >> 16)&0xff);
+  if (delta > 128){
+    delta -= 256;
+  }
+
+  if (delta < -128){
+    delta += 256;
+  }
+  
+  otoc = novy_otoc;
+
+  if (now - previous_update >= 400){
+      if (delta == 0)
+          return false;
+          
+      if (delta > 0){
+        if (player_snake->directions[0] == LEFT)
+          player_snake->directions[0] = DOWN;
+        else
+          player_snake->directions[0]++;
+      }
+      if (delta < 0)
+      {
+        if (player_snake->directions[0] == DOWN)
+          player_snake->directions[0] = LEFT;
+        else
+          player_snake->directions[0]--;
+      }
+    
+    return true;
+  }
+
+  return false;
+}
