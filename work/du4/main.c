5b9c8c46d91ace20d25d423e421e0a0f11982f06
Aidar Abdzhanov <abdzhaid@cvut.cz>
2025-05-30 15:58:12 +0200

    Delete work directory

--- work/du4/main.c
+++
@@ -1,8 +0,0 @@
-/* Testbed, no change expected in this file */
-
-int toplevel_fnc(void);
-
-int main(int argc, char *argv[])
-{
-  return toplevel_fnc();
-}
--- work/du4/subroutine.h
+++
@@ -1,3 +0,0 @@
-/* Header file which defines type of the subroutine subroutine_fnc */
-
-/* fill return type */ subroutine_fnc(/* fill in parameters */);
